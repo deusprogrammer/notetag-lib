99449e912f3973075d1943adc702e7b7bebfdf97
Michael <mike.main@rackspace.com>
2013-08-21 13:23:28 -0500

    Updated to allow module blocks for modules

--- notetag.rb
+++ notetag.rb
@@ -3,25 +3,27 @@ class NoteTag
     attr_reader   :attributes
     attr_accessor :children
     attr_accessor :data
-    attr_reader :has_children
+    
     def initialize(tagString)
         @label = ""
         @attributes = Hash.new
         @children = []
         @data = nil
-        openTagRegex = /<m:([a-zA-Z]+)(.*)>/
-        elementRegex = /(\w+)\s*=\s*['"](.+)['"]/
+        openTagRegex = /<(\S+)\s*(.*)>/
+        elementRegex = /(\w+)\s*=\s*['"]*([a-zA-Z0-9]+)['"]*/
         
-        print "TAG STRING: #{tagString}\n"
+        NoteTagBlock.print_debug "TAG STRING: #{tagString}\n"
         
         openTagElements = openTagRegex.match(tagString)
         if(!openTagElements.nil? && openTagElements.size > 0)
             @label = openTagElements[1]
             if (!openTagElements[2].nil? && openTagElements[2] != "")
                 openTagElements[2].split(/\s+/).each { |attribute|
-                    if (!attribute.nil? && attribute != "")
+                    if (!attribute.nil?)
                         values = elementRegex.match(attribute)
-                        @attributes[values[1]] = values[2]
+                        if (!values.nil? && values.size == 3)
+                            @attributes[values[1]] = values[2]
+                        end
                     end
                 }
             end
@@ -35,13 +37,35 @@ class NoteTag
     def has_data?
         return !data.nil?
     end
+    
+    def has_attributes?
+        return attributes.size > 0
+    end
+    
+    def is_module?
+        moduleRegex = /m:(\S+)/
+        modElements = moduleRegex.match(label)
+        if (modElements.nil? || modElements.size == 0)
+            return false
+        end
+        return true
+    end
+    
+    def module_name
+        moduleRegex = /m:(\S+)/
+        modElements = moduleRegex.match(label)
+        if (modElements.nil? || modElements.size == 0)
+            return nil
+        end
+        return modElements[1]
+    end
 end
 
 class CloseTag
     attr_reader :label
     def initialize(tagString)
         @label = ""
-        closeTagRegex = /<\/m:([a-zA-Z]+)>/
+        closeTagRegex = /<\/(\S+)>/
         
         closeTagElements = closeTagRegex.match(tagString)
         if(!closeTagElements.nil?)
@@ -63,22 +87,30 @@ class NoteTagBlock
     attr_reader :attributes
     attr_reader :note_tags
     
-    def initialize(noteTagString)
-        @note_tags = []
-        parseXml noteTagString
+    @@debug_mode = false
+    
+    def self.debug_mode=value
+        @@debug_mode = value
     end
     
-    def parseXml(noteTagString)
+    def self.print_debug(str)
+        if (@@debug_mode)
+            print str
+        end
+    end
+    
+    def initialize(noteTagString)
         # Read until a tag is read
+        @note_tags = []
         data = ""
         open_tag_data = ""
         open_tag = nil
         close_tag_data = ""
         state = :START
         
-        print "-------------------------------------------\n"
-        print "#{noteTagString}\n"
-        print "-------------------------------------------\n"
+        NoteTagBlock.print_debug "-------------------------------------------\n"
+        NoteTagBlock.print_debug "#{noteTagString}\n"
+        NoteTagBlock.print_debug "-------------------------------------------\n"
         
         noteTagString.each_char { |byte|
             # While in start state, look for start of first tag
@@ -88,24 +120,40 @@ class NoteTagBlock
                 if (byte == '<')
                     open_tag_data += byte
                     
-                    print "TRANSITION READING_OPEN_TAG\n"
+                    NoteTagBlock.print_debug "TRANSITION READING_OPEN_TAG\n"
                     state = :READING_OPEN_TAG
                 end
             when :READING_OPEN_TAG
                 open_tag_data += byte
                 if (byte == '>')
                     open_tag = NoteTag.new open_tag_data
-                    print "Searching for #{open_tag.label}\n"
-                    print "Attributes #{open_tag.attributes}\n"
+                    NoteTagBlock.print_debug "Searching for #{open_tag.label}\n"
+                    NoteTagBlock.print_debug "Attributes #{open_tag.attributes}\n"
                     
-                    print "TRANSITION SEARCH_CLOSE_TAG\n"
+                    NoteTagBlock.print_debug "TRANSITION SEARCH_CLOSE_TAG\n"
                     state = :SEARCH_CLOSE_TAG
+                elsif (byte == '/')
+                    open_tag = NoteTag.new open_tag_data + ">"
+                    # Add this child to note_tag and then parse remaining tags
+                    NoteTagBlock.print_debug "**********************************\n"
+                    NoteTagBlock.print_debug "Tag        #{open_tag.label}\n"
+                    NoteTagBlock.print_debug "Attributes #{open_tag.attributes}\n"
+                    NoteTagBlock.print_debug "**********************************\n"
+                    
+                    note_tags.push open_tag
+                    
+                    data = ""
+                    close_tag_data = ""
+                    open_tag_data = ""
+                    
+                    NoteTagBlock.print_debug "TRANSITION START\n"
+                    state = :START
                 end
             when :SEARCH_CLOSE_TAG
                 if (byte == '<')
                     close_tag_data += byte
                     
-                    print "TRANSITION VERIFY_CLOSE_TAG\n"
+                    NoteTagBlock.print_debug "TRANSITION VERIFY_CLOSE_TAG\n"
                     state = :VERIFY_CLOSE_TAG
                 else
                     data += byte    
@@ -116,31 +164,30 @@ class NoteTagBlock
                     data += close_tag_data
                     close_tag_data = ""
                     
-                    print "TRANSITION SEARCH_CLOSE_TAG\n"
+                    NoteTagBlock.print_debug "TRANSITION SEARCH_CLOSE_TAG\n"
                     state = :SEARCH_CLOSE_TAG
                 else
-                    print "TRANSITION READING_CLOSE_TAG\n"
+                    NoteTagBlock.print_debug "TRANSITION READING_CLOSE_TAG\n"
                     state = :READING_CLOSE_TAG
                 end
             when :READING_CLOSE_TAG
                 close_tag_data += byte
                 if (byte == '>')
                     close_tag = CloseTag.new close_tag_data
-                    print "**#{close_tag.label}** vs **#{open_tag.label}**\n"
+                    NoteTagBlock.print_debug "**#{close_tag.label}** vs **#{open_tag.label}**\n"
                     if (close_tag.label != open_tag.label)
                         data += close_tag_data
                         close_tag_data = ""
                         
-                        print "TRANSITION SEARCH_CLOSE_TAG\n"
+                        NoteTagBlock.print_debug "TRANSITION SEARCH_CLOSE_TAG\n"
                         state = :SEARCH_CLOSE_TAG
                     else
                         # Add this child to note_tag and then parse remaining tags
-                        print "**********************************\n"
-                        print "DATA:      #{data}\n"
-                        print "Tag        #{open_tag.label}\n"
-                        print "Attributes #{open_tag.attributes}\n"
-                        print "Note Tags  #{note_tags}\n"
-                        print "**********************************\n"
+                        NoteTagBlock.print_debug "**********************************\n"
+                        NoteTagBlock.print_debug "DATA:      #{data}\n"
+                        NoteTagBlock.print_debug "Tag        #{open_tag.label}\n"
+                        NoteTagBlock.print_debug "Attributes #{open_tag.attributes}\n"
+                        NoteTagBlock.print_debug "**********************************\n"
                         if (!data.nil? && data != "" && (data.include?("<") && data.include?(">")))
                             open_tag.children.push NoteTagBlock.new data
                         elsif (!data.nil? && data != "" && !(data.include?("<") || data.include?(">")))
@@ -153,7 +200,7 @@ class NoteTagBlock
                         close_tag_data = ""
                         open_tag_data = ""
                         
-                        print "TRANSITION START\n"
+                        NoteTagBlock.print_debug "TRANSITION START\n"
                         state = :START
                     end
                 end
@@ -161,41 +208,49 @@ class NoteTagBlock
             end
         }
     end
-end
-
-def print_note_tag_block(noteTagBlock, depth = 0)
-    noteTagBlock.note_tags.each {|note_tag|
-        depth.times {print "\t"}
-        print "TAG: #{note_tag.label}\n"
-        depth.times {print "\t"}
-        print "ATTRIBUTES:\n"
-        note_tag.attributes.each {|key, value|
+    
+    def debug(depth = 0)
+        note_tags.each {|note_tag|
             depth.times {print "\t"}
-            print "\t#{key} => #{value}\n"
+            if (note_tag.is_module?)
+                print "MODULE: #{note_tag.module_name}\n"
+            else
+                print "TAG:    #{note_tag.label}\n"
+            end
+            depth.times {print "\t"}
+            if (note_tag.has_attributes?)
+                print "ATTRIBUTES:\n"
+                note_tag.attributes.each {|key, value|
+                    depth.times {print "\t"}
+                    print "\t#{key} => #{value}\n"
+                }
+            end
+            depth.times {print "\t"}
+            if (note_tag.has_children?)
+                print "CHILDREN: "
+                note_tag.children.each {|child|
+                    print "\n"
+                    child.debug depth + 1
+                }
+            elsif (note_tag.has_data?)
+                print "DATA:   #{note_tag.data}\n"
+            end
+            print "\n"
         }
-        depth.times {print "\t"}
-        if (note_tag.has_children?)
-            print "CHILDREN: "
-            note_tag.children.each {|child|
-                print "\n"
-                print_note_tag_block child, depth + 1
-            }
-        elsif (note_tag.has_data?)
-            print "DATA: #{note_tag.data}\n"
-        end
-        print "\n"
-    }
+    end
 end
 
+#NoteTagBlock.debug_mode = true
 note_tag_block = NoteTagBlock.new   "<m:stats>" +
-                                        "<m:mp>30</m:mp>" +
-                                        "<m:skillSet type='magic'>" +
-                                            "<m:skill>Fire 1</m:skill>" +
-                                            "<m:skill>Fire 2</m:skill>" +
-                                        "</m:skillSet>" +
+                                        "<mp>30</mp>" +
+                                        "<skillSet type='magic'>" +
+                                            "<skill>Fire 1</skill>" +
+                                            "<skill>Fire 2</skill>" +
+                                        "</skillSet>" +
                                     "</m:stats>" +
                                     "<m:actorEquip>" +
-                                        "<m:actorSay actor='Melon'>I like this dress!</m:actorSay>" +
-                                        "<m:actorSay actor='Tess'>This isn't armor!</m:actorSay>" +
-                                    "</m:actorEquip>"
-print_note_tag_block note_tag_block
+                                        "<actorSay actor='Melon' slot='dress'>I like this dress!</actorSay>" +
+                                        "<actorSay actor='Tess' slot='dress'>This isn't armor!</actorSay>" +
+                                    "</m:actorEquip>" +
+                                    "<m:limitedUses uses=1 />"
+note_tag_block.debug
