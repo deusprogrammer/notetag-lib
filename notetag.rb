4db175378a1f08259cfbbb1b58bbbd48d1a8b7db
Michael <mike.main@rackspace.com>
2013-08-21 14:13:49 -0500

    Experimentation with reflection

--- notetag.rb
+++ notetag.rb
@@ -240,27 +240,37 @@ class NoteTagBlock
     end
 end
 
-#NoteTagBlock.debug_mode = true
-note_tag_block = NoteTagBlock.new   "<m:stats>" +
-                                        "<mp>30</mp>" +
-                                        "<skillSet type='magic'>" +
-                                            "<skill>Fire 1</skill>" +
-                                            "<skill>Fire 2</skill>" +
-                                        "</skillSet>" +
-                                    "</m:stats>" +
-                                    "<m:actorEquip>" +
-                                        "<actorSay actor='Melon' slot='dress'>I like this dress!</actorSay>" +
-                                        "<actorSay actor='Tess' slot='dress'>This isn't armor!</actorSay>" +
-                                    "</m:actorEquip>" +
-                                    "<m:limitedUses uses=1 />"
+note_tag_block_string = "<m:stats>" +
+                            "<mp>30</mp>" +
+                            "<skillSet type='magic'>" +
+                                "<skill>Fire 1</skill>" +
+                                "<skill>Fire 2</skill>" +
+                            "</skillSet>" +
+                        "</m:stats>" +
+                        "<m:actorEquip>" +
+                            "<actorSay actor='Melon' slot='dress'>I like this dress!</actorSay>" +
+                            "<actorSay actor='Tess' slot='dress'>This isn't armor!</actorSay>" +
+                        "</m:actorEquip>" +
+                        "<m:limitedUses uses=1 />"
+
+# Ability to get class name by name
+cls = Object.const_get('NoteTagBlock')
+note_tag_block = cls.new note_tag_block_string
 note_tag_block.debug
 
-print "OBJECTS: \n"
+# Reflection experimentation
+print "CLASS VARIABLES: \n"
+NoteTagBlock.class_variables.each {|variable|
+    print "\t#{variable}\n"
+}
+
+print "INSTANCE VARIABLES: \n"
 note_tag_block.instance_variables.each {|object|
     print "\t#{object} = #{note_tag_block.instance_variable_get(object)}\n"
 }
 
+# Get methods belonging to a object type
 print "METHODS: \n"
-note_tag_block.methods.each {|method|
+NoteTagBlock.methods.each {|method|
     print "\t#{method}\n"
 }
